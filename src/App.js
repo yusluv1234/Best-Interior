5e518be89a45c7b1f4fe27cf53a89af6bd24dcf6
yusluv1234 <yusluv124@gamil.com>
2020-11-24 15:02:19 +0100

    useer reducer

--- src/App.js
+++ src/App.js
@@ -16,20 +16,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
+      const { setCurrentUser } = this.props;
+
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
           });
         });
       }
-      this.setState({ currentUser: userAuth });
+      setCurrentUser(userAuth);
     });
   }
 
